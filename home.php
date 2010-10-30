0c4c7010a999d438067e033bd8212d0cfab69b75
Tunghsiao Liu <info@sparanoid.com>
2010-10-31 03:28:29 +0800

    Finished home page

--- home.php
+++ home.php
@@ -1,20 +1,159 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html dir=ltr lang=en>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="refresh" content="0; url=/wp-admin">
+<meta charset=utf-8>
+<meta name=viewport content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name=HandheldFriendly content=True>
 <title>nio2 / a.k.a. 89log</title>
+
 <style type="text/css">
 <!--
-body{background-image: url(images/v2_bg.gif); font-family:'Microsoft YaHei', 微软雅黑, verdana; font-size:18px; color:#333; text-shadow:#aaa 1px 1px 0;}
-#main{width:950px; margin:70px auto;}
+@font-face {
+	font-family: 'ChunkFive';
+	src: url('ff/ChunkFive/chunkfive-webfont.eot');
+	/* use smiley to ignore mac local fonts */
+	src: local('☺'), url('ff/ChunkFive/chunkfive.otf') format('opentype'), url('ff/ChunkFive/chunkfive-webfont.svg') format('svg');
+	font-weight: normal;
+	font-style: normal;
+}
+
+body {
+	background: yellow;
+	color: black;
+	font: normal normal normal 100%/1em ChunkFive, 'Helvetica Neue', Helvetica, Arial, STHeiti, 'Microsoft YaHei', sans-serif;
+	font-size: 48px;
+	-moz-font-feature-settings: "liga=1,dlig=1,hlig=1,calt=1,frac=1,salt=1,swsh=1,cswh=1";
+	     font-feature-settings: "liga=1,dlig=1,hlig=1,calt=1,frac=1,salt=1,swsh=1,cswh=1";
+	-webkit-font-smoothing: antialiased;
+	text-rendering: geometricPrecision;
+	overflow-x: hidden;
+}
+
+p, a {
+	color: #000;
+}
+
+p, h1 {
+	color: black;
+	text-shadow: 
+		rgba(255,0,0,0.1) 0 0 20px, 
+		rgba(255,0,0,0.1) 0 0 5px;
+}
+
+a {
+	text-decoration: none;
+/*	background: rgba(255,0,0,0.1);*/
+/*	padding: 1000px 0;*/
+}
+
+a:hover {
+	color: #fff;
+	background: rgba(255,0,0,0.1);
+/*	padding: 5px 15px 2560px;*/
+	padding: 20px 2560px 10px;
+	margin: 0 -2560px;
+}
+
+em {
+	font-family: Palatino;
+	font-style: italic;
+	text-shadow: 
+		rgba(255,255,0,1) 0 0 50px, 
+		rgba(255,255,0,1) 0 0 40px, 
+		rgba(255,255,0,1) 0 0 30px, 
+		rgba(255,255,0,1) 0 0 20px, 
+		rgba(255,255,0,1) 0 0 10px, 
+		rgba(255,255,0,1) 0 0 5px;
+	opacity: 0.75;
+	-webkit-user-select: none;
+	 -khtml-user-select: none; 
+	   -moz-user-select: none; 
+	        user-select: none;
+}
+
+#wrap {
+	max-width: 960px;
+	margin: 0 auto;
+	padding: 0 80px 40px 80px;
+}
+
+#hell-one,
+#hell-two,
+#hell-three {
+	letter-spacing: -0.02em;
+	word-spacing: 0.1em;
+	opacity: 0.6;
+}
+
+#hell-four {
+	opacity: 0.3;
+	font: normal normal normal 10px/1em 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	text-transform: uppercase;
+}
+
+#hell-one:hover,
+#hell-two:hover,
+#hell-three:hover,
+#hell-four:hover {
+	opacity: 1;
+}
+
+#hell-four a:hover {
+	color: white;
+	padding: 40px 2560px;
+}
+
+::-moz-selection {
+	background: rgba(255,0,0,0.1);
+	color: white;
+}
+
+::selection {
+	background: rgba(255,0,0,0.1);
+	color: white;
+}
+
+* {
+	-webkit-transition: all 0.15s ease-in-out; 
+	 -khtml-transition: all 0.15s ease-in-out; 
+	   -moz-transition: all 0.15s ease-in-out; 
+	     -o-transition: all 0.15s ease-in-out; 
+	        transition: all 0.15s ease-in-out;
+}
 -->
-</style></head>
+</style>
+
+<!--[if lt IE 9]>
+<script>(function(){if(!/*@cc_on!@*/0)return;var e = "abbr,article,aside,audio,bb,canvas,datagrid,datalist,details,dialog,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(',');for(var i=0;i<e.length;i++){document.createElement(e[i])}})()</script>
+<![endif]-->
+
+</head>
 
 <body>
-<div id="main">
-<p>正在转向至控制面板...<br />Now redirecting to UCP...<p>
+
+<div id=wrap>
+
+<header id=hell-one>
+	<h1><a href="//nio2.com" title="nio2">nio2</a></h1>
+</header>
+
+<p id=hell-two>
+	Redirecting to <a href="//nio2.com/wp-admin" title="Go to dashboard">dashboard</a> in 10 secs. 
+</p>
+
+<p id=hell-three>
+	Another personal blog provider driven by users, 
+	powered by <a href="http://wordpress.org" title="WordPress">WordPress</a> 
+	<em>&amp;</em> <a href="//sparanoid.com" title="a.k.a Sparanoid">Tunghsiao Liu</a>. Read the latest news at 
+	<a href="//nio2.com/dev" title="Development log">Development log</a>.
+</p>
+
+<footer id=hell-four>
+	<p>&copy; A <a href="">Sparanoid</a> production. Best view on a &#xf8ff;</p>
+</footer>
+
 </div>
 
 </body>
+
 </html>

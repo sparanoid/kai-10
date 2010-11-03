1ce844226118ea88914ae1dda3376fe6c06b9ee9
Tunghsiao Liu <info@sparanoid.com>
2010-11-04 02:53:11 +0800

    Added missing file

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+// Override width of Twenty Ten
+define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyten_header_image_width', 840 ) );
+define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyten_header_image_height', 198 ) );
+
+?>
---
+++ page-archives.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php if ( is_front_page() ) { ?>
+						<h2 class="entry-title"><?php the_title(); ?></h2>
+					<?php } else { ?>
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php } ?>
+
+					<div class="entry-content">
+						<h2><?php _e( 'Tag Cloud', 'twentyten_kai' ); ?></h2>
+									<p id="tag-cloud"><?php wp_tag_cloud('smallest=9&largest=30&unit=px&order=ASC&number=0'); ?></p>
+						
+									<h2><?php _e( 'Archive list', 'twentyten_kai' ); ?></h2>
+									<ul>
+									<?php query_posts('showposts=-1');
+									if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+									<li class="post-title"><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
+									<?php endwhile; else: ?>
+									<li>Nothing Found!</li>
+									<?php endif; ?>
+									<?php wp_reset_query(); ?>
+									</ul>
+						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-## -->
+
+				<?php comments_template( '', true ); ?>
+
+<?php endwhile; ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>

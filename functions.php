792b8586598d18b0cc8d8c79f30bf12997bbb77c
Tunghsiao Liu <info@sparanoid.com>
2010-11-04 03:06:14 +0800

    Style updated

--- functions.php
+++ functions.php
@@ -4,3 +4,5 @@ define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyten_header_image_width', 840
 define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyten_header_image_height', 198 ) );
 
 ?>
+
+<?php get_avatar( $id_or_email, $size = '96', $default = '<path_to_url>' ); ?>

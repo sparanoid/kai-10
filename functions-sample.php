5cbb39dd85b07f3ce4aa28e1d4714e1b32b21fc7
Tunghsiao Liu <info@sparanoid.com>
2011-02-25 18:47:05 +0800

    UPDATE file structure, BUMP version to 1.9

--- functions-sample.php
+++ functions-sample.php
@@ -1,7 +1,6 @@
 <?php
 
 // Override the header width of Twenty Ten
-
 define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyten_header_image_width', 840 ) );
 define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyten_header_image_height', 198 ) );
 

435d989bd557dafcb47ee31b88b010f3569f2e21
Tunghsiao Liu <info@sparanoid.com>
2012-11-21 10:33:14 +0800

    Remove Infinite Scroll compatibility code since Jetpack 2.0.1 Infinite Scroll checks a child theme's parent theme for infinite scroll support.

--- functions.php
+++ functions.php
@@ -20,39 +20,4 @@ function twentyten_scripts_styles_override() {
    wp_enqueue_style( 'twentyten-kai-style', get_stylesheet_directory_uri() . '/core.css' );
  }
 add_action( 'wp_enqueue_scripts', 'twentyten_scripts_styles_override' );
-
-/**
- * Add theme support for infinity scroll
- *
- * @since Twenty Ten Kai 2.2.2
- */
-function twenty_ten_kai_infinite_scroll_init() {
-  add_theme_support( 'infinite-scroll', array(
-    'container' => 'content',
-    'render'    => 'twenty_ten_kai_infinite_scroll_render',
-    'footer'    => 'wrapper'
-  ) );
-}
-add_action( 'init', 'twenty_ten_kai_infinite_scroll_init' );
-
-/**
- * Set the code to be rendered on for calling posts,
- * hooked to template parts when possible.
- *
- * Note: must define a loop.
- */
-function twenty_ten_kai_infinite_scroll_render() {
-  get_template_part( 'loop' );
-}
-
-/**
- * Do we have footer widgets?
- */
-function twenty_ten_has_footer_widgets( $has_widgets ) {
-  if ( is_active_sidebar( 'first-footer-widget-area' ) || is_active_sidebar( 'second-footer-widget-area' ) || is_active_sidebar( 'third-footer-widget-area'  ) || is_active_sidebar( 'fourth-footer-widget-area' ) )
-    $has_widgets = true;
-
-  return $has_widgets;
-}
-add_filter( 'infinite_scroll_has_footer_widgets', 'twenty_ten_has_footer_widgets' );
 ?>
